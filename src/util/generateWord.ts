1f4e4090fe8b5eacadbe2578c4999f7ff887dc4c
jdunn99 <jmhjdunn123@outlook.com>
2022-02-09 11:52:06 -0500

    fix unused import

--- src/util/generateWord.ts
+++ src/util/generateWord.ts
@@ -1,11 +1,11 @@
 import { VALID_WORDS } from "./dictionary";
 
 const generateWord = () => {
-  const epochMs = new Date("January 1, 2022 00:00:00").valueOf();
+  const start = new Date("January 1, 2022 00:00:00").valueOf();
   const now = Date.now();
+
   const msInDay = 86400000;
-  const index = Math.floor((now - epochMs) / msInDay);
-  const nextday = (index + 1) * msInDay + epochMs;
+  const index = Math.floor((now - start) / msInDay);
 
   return VALID_WORDS[index % VALID_WORDS.length].toUpperCase();
 };
